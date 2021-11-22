780f00c0f3ddfec9db6a405501ea9047d773b66b
Rebecca <rebeccakatemansell@gmail.com>
2021-11-22 18:17:01 +1000

    start of portfolio!

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: {
+    enabled: false,
+    content: [
+      './index.html',
+      './js.js',
+    ],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
